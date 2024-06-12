51b33415f96be0b7490c72380444b3cc8b57f165
Kaleb <gabrielaragaosampaio@gmail.com>
2024-06-12 10:05:38 -0300

    ajuste no bd

--- dis.sql
+++ dis.sql
@@ -36,7 +36,7 @@ CREATE TABLE `clientes` (
   `id_duplicatas` int DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `senha` varchar(30) DEFAULT NULL,
-  `idade` int NOT NULL
+  `idade` int 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
