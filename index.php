7075d7edde7867ccc743e6aa23089cb2dd975509
kaleb <gabrielaragaosampaio@gmail.com>
2024-08-09 23:02:06 -0300

    ajuste de design da parte de login, separação total do html do css, aprimoramentos do css e responsividade

--- index.php
+++ index.php
@@ -4,87 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/log.css">
-    <style>
-
-   header {
-   background-color: #26a500;
-   color: #fff;
-   padding: 10px;
-   text-align: center;
-   }  
-
-   footer {
-    background-color: #26a500;
-    color: #fff;
-    padding: 0px;
-    text-align: center;
-    width: 100%;
-    height: 35px;
-    position: fixed;
-    bottom: 0;
-}
-    </style>
     <title>Log In</title>
 </head>
 <body>
 <header>
-        <div class="main-title"><h1>Supermercado Online</h1></div>
-            </div>
-        </div>
-   </header>
-
+    <h1>Supermercado Online</h1>
+</header>
 <div class="wrapper">
-
-   <form action="index.php" method="post">
-
-        <div class="card-switch">
-            <label class="switch">
-               <input type="checkbox" class="toggle">
-               <span class="slider"></span>
-               <span class="card-side"></span>
-               <div class="flip-card__inner">
-                  <div class="flip-card__front">
-                     <div class="title">Log in</div>
-                     <form class="flip-card__form" action="">
-                        
-                        <input class="flip-card__input" name="nome" placeholder="Nome" type="name">
+    <div class="card-switch">
+        <label class="switch">
+            <input type="checkbox" class="toggle">
+            <span class="slider"></span>
+            <span class="card-side"></span>
+          
+            <div class="flip-card__inner">
+                <div class="flip-card__front">
+                    <div class="title">Log in</div>
+                    <form class="flip-card__form" action="index.php" method="post">
+                        <input class="flip-card__input" name="nome" placeholder="Nome" type="text">
                         <input class="flip-card__input" name="email" placeholder="Email" type="email">
                         <input class="flip-card__input" name="senha" placeholder="Password" type="password">
                         <button class="flip-card__btn">Vamos lá!</button>
-                        </form>
+                    </form>
+                    <?php
 
-                        <?php
+                     include 'verificar/verificarClientes.php';
 
-                        include 'verificar/verificarClientes.php';
+                     ?>
 
-                        ?>
+                </div>
 
-                     </form>
-                     
-                     <form action="verificar/verificarCad.php" method="post">
-                  </div>
-                  
-                  <div class="flip-card__back">
-                     <div class="title">Cadastro</div>
-                     <br>
-                     <form class="flip-card__form" action="">
-                        <input class="flip-card__input" placeholder="Nome" type="name" name="nome">
+                <div class="flip-card__back">
+                    <div class="title">Cadastro</div>
+                    <form class="flip-card__form" action="verificar/verificarCad.php" method="post">
+                        <input class="flip-card__input" name="nome" placeholder="Nome" type="text">
                         <input class="flip-card__input" name="email" placeholder="Email" type="email">
                         <input class="flip-card__input" name="senha" placeholder="Senha" type="password">
-                        <br>
                         <button class="flip-card__btn">Confirmar!</button>
-                        <br>
-                     
-
-                  </div>
-               </div>
-               </form>
-            </label>
-        </div>   
-        </form>
-   </div>
-   <footer>
-        <p>&copy; Feito por Valter, Brena e VJ. Todos os direitos reservados.</p>
-    </footer>
+                    </form>
+                    
+                </div>
+            </div>
+        </label>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer>
+    <p>&copy; Feito por Valter, Brena e VJ. Todos os direitos reservados.</p>
+</footer>
 </body>
 </html>
--- verificar/conexao.php
+++ verificar/conexao.php
@@ -1,16 +1,13 @@
 <?php
-    // Configurações de conexão com o banco de dados
-    $servername = "localhost"; // Nome do servidor MySQL (normalmente 'localhost')
-    $username = "root"; // Nome de usuário do MySQL
-    $password = "mint"; // Senha do MySQL
-    $dbname = "dis"; // Nome do banco de dados
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "dis"; 
 
-    // Conexão com o banco de dados
+
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Verifica se houve algum erro na conexão
     if ($conn->connect_error) {
         die("Falha na conexão: " . $conn->connect_error);
     }
-    // include_once "apagar.php";
     ?>
